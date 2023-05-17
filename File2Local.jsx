28df38978443480f20c60b1ee530e733acee97c2
ddDanYY <132532006+ddDanYY@users.noreply.github.com>
2023-05-17 11:42:36 +0800

    Update File2Local.jsx

--- File2Local.jsx
+++ File2Local.jsx
@@ -9,36 +9,41 @@ if(!assetsFolder.exists) {
 
 // File in project
 var projectItems = app.project.items;
-var Extension = ["png", "jpg" , "exr" , "gif"];
+var Extension = ["png", "jpg", "exr", "gif"];
 var SeqText = [];
 
-// Check items and exclude sequence image files with png, jpg, exr, and gif extensions
+
 for(var i = 1; i <= projectItems.length; i++) {
     var item = projectItems[i];
-    var fileExtension = item.file.name.split('.').pop();
+    var fileExtension = item.name.split('.').pop();
     var fileLowCaseExten = fileExtension.toLowerCase();
-
-    if (item.mainSource.isStill == false){ 
-      if(fileLowCaseExten !== Extension[0] && fileLowCaseExten !== Extension[1] && fileLowCaseExten !== Extension[2] && fileLowCaseExten !== Extension[3]){
+    if (item.duration == 0){ 
+        CopyFile(FolderName);
+      }else if(item.duration !== 0 && CheckExtension(fileLowCaseExten) == false){
         CopyFile(FolderName);
       }else{
         SeqText.push(0)
       }
-}else{
-  CopyFile(FolderName);
-}
-}
+    }
 
-if(SeqText[0] ==0){
+if(SeqText.length > 0){
 alert("Doesn't Support Image Sequences!");
 }
 
+// Check items and exclude sequence image files with png, jpg, exr, and gif extensions
+function CheckExtension(fileLowCaseExten){
+if(fileLowCaseExten == Extension[0] || fileLowCaseExten == Extension[1] || fileLowCaseExten == Extension[2] || fileLowCaseExten == Extension[3]){
+  return true;
+}else{
+  return false;
+}
+}
 
 // Copy and replace
 function CopyFile(FolderName){
-  if(item instanceof FootageItem) {
+  if(item instanceof FootageItem && item.mainSource instanceof FileSource) {
     // Get filename
-    var fileName = item.file.name;
+    var fileName = item.file.displayName;
     // Copy File
     var newPath = app.project.file.parent.fsName + "\\" + FolderName + "\\" + fileName;
     if(!File(newPath).exists) {
@@ -48,5 +53,3 @@ function CopyFile(FolderName){
     item.replace(File(newPath));
 }
 }
-
-
